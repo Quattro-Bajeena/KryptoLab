503031d6bdc98a8793d6a08a29f62a39ef690fd9
Quattro-Bajeena <motmen001@gmail.com>
2023-05-05 15:26:44 +0200

    hash functions

---
+++ Hashing/Program.cs
@@ -0,0 +1,218 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+
+using SHA3.Net;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Hashing
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length > 0)
+			{
+				PrintHashes(args[0]);
+			}
+			else
+			{
+				//MeasureTimes();
+				//ShortWordHash();
+				//FindCollisions();
+				MeasureSAC();
+				
+			}
+			
+		}
+
+		const string filesDir = "test_files";
+		
+		static void PrintHashes(string word)
+		{
+			var word_bytes = Encoding.ASCII.GetBytes(word);
+			using var md5 = MD5.Create();
+			using var sha1 = SHA1.Create();
+			using var sha3 = Sha3.Sha3384();
+
+			var md5_hash = md5.ComputeHash(word_bytes);
+			var sha1_hash = sha1.ComputeHash(word_bytes);
+			var sha3_hash = sha3.ComputeHash(word_bytes);
+
+			Console.WriteLine("Hashed value: " + word);
+			Console.WriteLine("MD5:   " + Convert.ToHexString(md5_hash));
+			Console.WriteLine("SHA-1: " + Convert.ToHexString(sha1_hash));
+			Console.WriteLine("SHA-3: " + Convert.ToHexString(sha3_hash));
+
+		}
+
+
+		static void MeasureTimes()
+		{
+			var test_files = new DirectoryInfo(filesDir).GetFiles().OrderBy(f => f.Length);
+			var sw = new Stopwatch();
+
+
+
+			var times = new Dictionary<string, List<(string, long)>>() {
+				{ "md5",  new () }, 
+				{ "sha1", new () },
+				{ "sha3", new () }
+			};
+
+			foreach (var file in test_files)
+			{
+				
+				using var md5 = MD5.Create();
+				using var sha1 = SHA1.Create();
+				using var sha3 = Sha3.Sha3384();
+
+				var file_description = "File: " + file.Name + " Size: " + file.Length;
+				Console.WriteLine(file_description);
+
+				using var fs1 = file.OpenRead();
+				sw.Start();
+				var md5_hash = md5.ComputeHash(fs1);
+				times["md5"].Add( (file.Name, sw.ElapsedMilliseconds) );
+
+				using var fs2 = file.OpenRead();
+				sw.Restart();
+				var sha1_hash = sha1.ComputeHash(fs2);
+				times["sha1"].Add((file.Name, sw.ElapsedMilliseconds));
+
+				using var fs3 = file.OpenRead();
+				sw.Restart();
+				var sha3_hash = sha3.ComputeHash(fs3);
+				times["sha3"].Add((file.Name, sw.ElapsedMilliseconds));
+			}
+
+			foreach(var algo_time in times)
+			{
+				Console.WriteLine(algo_time.Key);
+				foreach(var time in algo_time.Value)
+				{
+					Console.WriteLine(time.Item1 + ": " + time.Item2 + "ms");
+				}
+			}
+
+
+		}
+
+		static void ShortWordHash()
+		{
+			var word = "ares";
+			using var md5 = MD5.Create();
+
+			var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(word));
+			Console.WriteLine(Convert.ToHexString(hash));
+		}
+
+		static void FindCollisions()
+		{
+			using var md5 = MD5.Create();
+			var no_hashes = 1_000;
+
+			var randomStrings = Enumerable.Range(0, no_hashes).Select(i => Guid.NewGuid().ToString()).ToArray();
+			var hashedStrings = randomStrings
+				.Select(
+					str => new BitArray(md5.ComputeHash(Encoding.UTF8.GetBytes(str)))
+				)
+				.ToArray();
+
+			
+
+			for (int i = 1; i < md5.HashSize; i++)
+			{
+				var hashedPrefixes = hashedStrings.Select(bits => GetFirstNBits(bits, i)).ToArray();
+
+				var collisionsFound = hashedPrefixes.Length - hashedPrefixes.Distinct(new BitArrayEqualityComparer()).ToArray().Length;
+
+				Console.WriteLine("Collisions on first " + i + " bits: " + collisionsFound);
+				
+			}
+		}
+
+		class BitArrayEqualityComparer : IEqualityComparer<BitArray>
+		{
+			public bool Equals(BitArray? x, BitArray? y)
+			{
+				if (x == null && y == null) return true;
+				if (x == null || y == null) return false; 
+				if (x.Length != y.Length) return false;	
+				return x.Cast<bool>().SequenceEqual(y.Cast<bool>());
+			}
+
+			public int GetHashCode([DisallowNull] BitArray obj)
+			{
+				var bools = obj.Cast<bool>().ToArray();
+				int hash = 17;
+				for (int index = 0; index < bools.Length; index++)
+				{
+					hash = hash * 23 + bools[index].GetHashCode();
+				}
+				return hash;
+			}
+		}
+
+
+		static BitArray GetFirstNBits(BitArray bits, int n)
+		{
+			var first_n = new bool[n];
+			for(int i = 0; i < n; i++)
+			{
+				first_n[i] = bits.Get(i);
+			}
+
+			var output = new BitArray(first_n);
+			return output;
+		}
+
+		static float SAC()
+		{
+			using var md5 = MD5.Create();
+			var value = Guid.NewGuid().ToString();
+			var value_bytes = Encoding.ASCII.GetBytes(value);
+			var value_bits = new BitArray(value_bytes);
+
+			var first_hash = md5.ComputeHash(value_bytes);
+			var first_hash_bits = new BitArray(first_hash);
+
+
+			var value_modified_bits = (BitArray) value_bits.Clone();
+			value_modified_bits.Set(0, !value_modified_bits[0]);
+
+			var value_modified_bytes = new byte[value_bytes.Length];
+			value_modified_bits.CopyTo(value_modified_bytes, 0);
+
+			var second_hash = md5.ComputeHash(value_modified_bytes);
+			var second_hash_bits = new BitArray(second_hash);
+
+			var hashes_xor = first_hash_bits.Xor(second_hash_bits);
+			var changed_bits = 0;
+			foreach(bool bit in hashes_xor)
+			{
+				changed_bits += bit ? 1 : 0;
+			}
+
+			var changed_bit_ratio = (float)changed_bits / first_hash_bits.Count;
+			//Console.WriteLine(changed_bit_ratio);
+			return changed_bit_ratio;
+
+		}
+
+		static void MeasureSAC()
+		{
+			var ratios = new List<float>();
+			for (int i = 0; i < 1000000; i++)
+			{
+				ratios.Add(SAC());
+			}
+			Console.WriteLine("Min: " + ratios.Min());
+			Console.WriteLine("Max: " + ratios.Max());
+			Console.WriteLine("Average: " + ratios.Average());
+		}
+	}
+}
---
+++ hashing.md
@@ -0,0 +1,55 @@
+# Funkcje Skrótów 
+
+Autor: *Mateusz Oleszek*, nr. 144608
+
+Wybrane funkcje do testowania: MD5, SHA-1, SHA-3
+
+
+
+## Skrót za pomocą różnych algorytmów
+
+![](F:\Programowanie\Studia\KryptoLab\screenshots\Screenshot_6151_WindowsTerminal-2023_05_05-15_23.png)
+
+## Szybkość działania
+
+<img src="F:\Programowanie\Studia\KryptoLab\screenshots\Screenshot_6149_EXCEL-2023_05_05-13_40.png" style="zoom:50%;" />
+
+Najszybsza jest funkcja SHA-1, potem MD5, a na końcu SHA-3. 
+
+## Funkcja MD5 dla krótkiego słowa
+
+Skrót MD5 dla słowa wejściowego "ares": 1769d06df18cb4c2b01931d7f83f3c9a
+
+Wyniki po wyszukaniu tego skrótu w google
+
+<img src="F:\Programowanie\Studia\KryptoLab\screenshots\Screenshot_6146_firefox-2023_05_05-13_18.png" style="zoom:33%;" />
+
+
+
+<img src="F:\Programowanie\Studia\KryptoLab\screenshots\Screenshot_6147_firefox-2023_05_05-13_18.png" style="zoom:33%;" />
+
+
+
+## Czy funkcja skrótu nie jest bezpieczna
+
+Funkcja MD5 jest powszechnie uznawana za złamaną kryptograficznie i nienadającą się do użytkowania od kilkunastu lat. Konsumenckie karty graficzne potrafią generować miliony skrótów na sekundę.
+
+Zostały też znalezione ciągi które posiadają ten sam skrót, a co więcej nawet algorytm pozwalający stworzyć 2 pliki o dowolnej długości które będą miały ten sam skrót.
+
+
+
+## Znajdowanie kolizji na pierwszych bitach
+
+Dla funkcji skrótów wygenerowanych z **miliona** losowych ciągów znaków kolizje były znalezione na pierwszych **40 bitach**. Im więcej skrótów było generowanych tym można było znaleźć kolizję dla większej liczby bitów, np. dla tysiąca było to tylko pierwsze 19 bitów. Tak więc dla większych zbiorów skrótów będzie można znaleźć coraz bardziej podobne skróty z większym prawdopodobieństwem.
+
+
+
+## Kryterium SAC
+
+Zostały przetestowane milion losowych ciągów znaków, w których został zmieniony jeden bit a potem porównane bity skrótów oryginalnej i zmienionej wartości. Zaobserwowane procenty zmienionych bitów pomiędzy nimi to:
+
+- Min: ~29%
+- Max: ~72%
+- Średnia: 50.001%
+
+Patrząc na średnią wartość będącą prawie dokładnie 50% można uznać, że ten algorytm spełnia kryterium SAC.
